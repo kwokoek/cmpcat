4e6b2778f736143ab9163719c43fcbb1bec0b565
kwokoek <kwokoek@glgroup.com>
2013-01-15 08:11:24 -0600

    Test driver

---
+++ run_set.bat
@@ -0,0 +1,10 @@
+if test $# != 1 ; then
+echo "Run set requires 1 argument"
+echo "Usage :"
+echo " run_set.bat <run_name>"
+exit 1
+fi
+
+echo "Running test set $1"
+./cmp_api.test.bat 473782 $1 a3dva29la0BnbGdkZXYuY29tOkdsZ3JvdXAxMjM=
+./cmp_api.test.bat 643305 $1 dGVzdDJAZ2xnZGV2LmNvbTpHbGdyb3VwMTIz
