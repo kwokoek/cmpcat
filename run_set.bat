f1219ea5fe5ee8fe561ea477dad78d25c6cff70d
kwokoek <kwokoek@glgroup.com>
2014-04-10 13:56:05 -0500

    Update stat

--- run_set.bat
+++ run_set.bat
@@ -5,6 +5,8 @@ echo " run_set.bat <run_name>"
 exit 1
 fi
 
+// sample run sets
+
 echo "Running test set $1"
 ./cmp_api.test.bat 473782 $1 a3dva29la0BnbGdkZXYuY29tOkdsZ3JvdXAxMjM=
 ./cmp_api.test.bat 643305 $1 dGVzdDJAZ2xnZGV2LmNvbTpHbGdyb3VwMTIz
